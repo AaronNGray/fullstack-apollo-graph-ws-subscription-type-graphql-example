c00552778091a449ae2b7b6e1f4df8e635d72c8d
Aaron Gray <aaronngray@gmail.com>
2021-09-14 18:59:51 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,27 @@
+# fullstack-apollo-graphql-ws-subscription-test
+
+A minimal Apollo Server 3 with Apollo Client 3 application with subscriptions.
+
+## Installation
+
+* `git clone https://github.com/AaronNGray/fullstack-apollo-graphql-ws-subscription-test.git`
+* `cd fullstack-apollo-graphql-ws-subscription-example`
+
+### Server
+
+* `cd server`
+* `npm install`
+* `npm start`
+
+### Client
+
+* `cd client`
+* `npm install`
+* `npm start`
+* visit `http://localhost:3000`
+
+## Want to learn more about React + GraphQL + Apollo + graphql-ws ?
+
+* Don't miss [upcoming Tutorials and Courses](https://www.getrevue.co/profile/rwieruch)
+* Check out current [React Courses](https://roadtoreact.com)
+* [graphql-ws](https://github.com/enisdenjo/graphql-ws)
---
+++ client/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { getMainDefinition } from 'apollo-utilities';
+import { ApolloLink, split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { createClient } from 'graphql-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+
+import App from './App';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8000/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:8000/graphql`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const sub_client = createClient({
+    url: 'ws://localhost:4000/subscriptions',
+});
+
+const terminatingLink = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' && operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const link = ApolloLink.from([terminatingLink]);
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client} sub_client={sub_client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
